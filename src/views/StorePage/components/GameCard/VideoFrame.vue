2534e38864b21a8f02777e5d93e85ec69a0c017b
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-09-15 15:55:46 +0900

    Fixed embedded youtube bug

--- src/views/StorePage/components/GameCard/VideoFrame.vue
+++ src/views/StorePage/components/GameCard/VideoFrame.vue
@@ -1,13 +1,12 @@
 <template lang="">
 	<iframe
 		v-if="videoURL"
-		autoplay="1"
-		controls="0"
-		:src="`https://www.youtube.com/embed/` + videoURL"
+		id="ytplayer"
+		type="text/html"
+		:src="videoURL"
 		title="Youtube Trailer"
-		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allow="autoplay"
 		frameborder="0"
-		scrolling="no"
 		allowfullscreen
 		class="trailer-video"
 	></iframe>
@@ -20,10 +19,7 @@ const props = defineProps({
 	videoID: String,
 })
 
-console.log("LOADED THE VIDEO GRAME")
-console.log(props.videoID)
-
-const videoURL = ref<string | undefined>(props.videoID)
+const videoURL = ref<string | undefined>(`https://www.youtube.com/embed/${props.videoID}?autoplay="1"&controls="0"`)
 </script>
 
 <style lang="scss" scoped>
